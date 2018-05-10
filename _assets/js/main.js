0ba817395b036ade32a974e0436c5f1487830c3b
Alex Robertson-Brown <alex.robertsonbrown@gmail.com>
2018-05-10 10:24:48 +0100

    first round changes

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,1 +1,19 @@
 // general js for the project that wouldn't be a reuseable component
+
+$('.js-map-hover-active').add('.map__marker').on('mouseover', function() {
+  $('.js-map-banner').addClass('is-active');
+})
+
+$('.js-map-hover-active').add('.map-banner__wrap').on('mouseout', function() {
+  $('.js-map-banner').removeClass('is-active');
+})
+
+
+$('.js-open-map').on('click', function () {
+  $('.js-magic-button').remove();
+  $('.js-map-banner').addClass('is-open');
+})
+
+$('.js-close-map').on('click', function () {
+  $('.js-map-banner').removeClass('is-open');
+})
